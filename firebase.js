77aea653ddbd1aaaf9c8348cc207a84c94ef4d50
Luis Modesto <75289823+RepellantHail@users.noreply.github.com>
2023-06-15 14:43:06 -0600

    Alivianese padrino

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 const doctores = []
 const consultas = []
-const doctorContainer = document.querySelector(".elements-container")
+const doctorContainer = document.getElementById("elements-container")
 const collectionName = "doctores"
 
 // Import the functions you need from the SDKs you need
@@ -66,18 +66,13 @@ const handleDataDocs = () => {
 };
 
 function crearDoctor(doctor,consultaID){
-    const row = document.createElement('div')
-    row.classList.add('row')
-    row.classList.add('row-cols-1')
-    row.classList.add('row-cols-md-3')
-    row.classList.add('g-4')
-    
+
     const divContainer = document.createElement('div')
     divContainer.classList.add('col')
 
     const cardContainer = document.createElement('div')
     cardContainer.classList.add('card-container')
-
+    
     const card = document.createElement('div')
     card.classList.add("card")
     card.classList.add("text-bg-secondary")
@@ -107,14 +102,33 @@ function crearDoctor(doctor,consultaID){
     cedula.style.padding = "0px"
     cedula.classList.add('card-text')
     cedula.classList.add('fs-4')
-    cedula.textContent = `Cedula : ${doctor.cedula}`
+    cedula.innerHTML = `<i class="fa-solid fa-certificate"></i> Cedula : ${doctor.cedula}`
 
     const especialidad = document.createElement('p')
     especialidad.style.margin = "0px"
     especialidad.style.padding = "0px"
     especialidad.classList.add('card-text')
     especialidad.classList.add('fs-4')
-    especialidad.textContent = `Especialidad: ${doctor.especialidad}`
+    especialidad.innerHTML = `<i class="fa-solid fa-stethoscope"></i> Especialidad : ${doctor.especialidad}`
+
+    const btnGroup = document.createElement('div')
+    btnGroup.classList.add('btn-group')
+    btnGroup.classList.add('p-1')
+    btnGroup.setAttribute('role','button')
+
+    const btnActualizar = document.createElement('button')
+    btnActualizar.classList.add('btn')
+    btnActualizar.classList.add('btn-warning')
+    btnActualizar.setAttribute('type','submit')
+    btnActualizar.setAttribute('data-bs-toggle','modal')
+    btnActualizar.setAttribute('data-bs-target','#editModal')
+    btnActualizar.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>'
+
+    const btnEliminar = document.createElement('button')
+    btnEliminar.classList.add('btn')
+    btnEliminar.classList.add('btn-danger')
+    btnEliminar.setAttribute('type','submit')
+    btnEliminar.innerHTML = '<i class="fa-solid fa-trash"></i>'
 
     const button = document.createElement("button");
     button.textContent = "Consultas";
@@ -125,6 +139,9 @@ function crearDoctor(doctor,consultaID){
         retrieveSecondCollectionData(consultaID);
     });
 
+    btnGroup.appendChild(btnActualizar)
+    btnGroup.appendChild(btnEliminar)
+
     cardListElement.appendChild(cedula)
     cardListElement.appendChild(especialidad)
 
@@ -133,12 +150,76 @@ function crearDoctor(doctor,consultaID){
 
     card.appendChild(cardTitle)
     card.appendChild(cardBody)
+    card.appendChild(btnGroup)
     card.appendChild(button);
 
     cardContainer.appendChild(card)
-    row.appendChild(cardContainer)
-    doctorContainer.appendChild(row)
+    doctorContainer.appendChild(crearModalEdit(doctor.nombre,doctor.cedula,doctor.especialidad))
+    doctorContainer.appendChild(cardContainer)
 }
+
+function crearModalEdit(nombre,cedula,especialidad){
+
+    // Modal
+
+    const divModalFade = document.createElement('div')
+    divModalFade.classList.add('modal')
+    divModalFade.classList.add('fade')
+    divModalFade.setAttribute('id', 'editModal')
+    divModalFade.setAttribute('tabindex', '-1')
+    divModalFade.setAttribute('aria-hidden', 'true')
+
+    const modalDialog = document.createElement('div')
+    modalDialog.classList.add('modal-dialog')
+
+    const modalContent = document.createElement('div')
+    modalContent.classList.add('modal-content')
+
+    const modalHeader = document.createElement('div')
+    modalHeader.classList.add('modal-header')
+
+    const modalTitle = document.createElement('h2')
+    modalTitle.classList.add('modal-title')
+    modalTitle.classList.add('fs-5')
+    modalTitle.innerHTML = nombre
+
+    const modalEditBtnClose = document.createElement('button')
+    modalEditBtnClose.classList.add('btn-close')
+    modalEditBtnClose.setAttribute('data-bs-dismiss','modal')
+    modalEditBtnClose.setAttribute('aria-label','Close')
+
+    const editModalBody = document.createElement('div')
+    editModalBody.classList.add('modal-body')
+    editModalBody.innerHTML = `<input class="form-control" value="${nombre}"><input class="form-control" value="${cedula}"><input class="form-control" value="${especialidad}">`
+
+    const editModalFooter = document.createElement('div')
+    editModalFooter.classList.add('modal-footer')
+
+    const btnCloseModalFooter = document.createElement('button')
+    btnCloseModalFooter.classList.add('btn')
+    btnCloseModalFooter.classList.add('btn-secondary')
+    btnCloseModalFooter.setAttribute('data-bs-dismiss','modal')
+    btnCloseModalFooter.innerText = "Cerrar"
+
+    const btnGuardarModalFooter = document.createElement('button')
+    btnGuardarModalFooter.classList.add('btn')
+    btnGuardarModalFooter.classList.add('btn-primary')
+    btnGuardarModalFooter.innerText = "Guardar Cambios"
+
+    editModalFooter.appendChild(btnCloseModalFooter)
+    editModalFooter.appendChild(btnGuardarModalFooter)
+    modalHeader.appendChild(modalTitle)
+    modalHeader.appendChild(modalEditBtnClose)
+    modalContent.appendChild(modalHeader)
+    modalContent.appendChild(editModalBody)
+    modalContent.appendChild(editModalFooter)
+    modalDialog.appendChild(modalContent)
+    divModalFade.appendChild(modalDialog)
+    
+    // End of modal
+    return  divModalFade
+}
+
 
 const retrieveSecondCollectionData = async (doctorId) => {
     const querySnapshot = await getDocs(
--- firebasePacientes.js
+++ firebasePacientes.js
@@ -1,5 +1,5 @@
 const pacientes = []
-const doctorContainer = document.querySelector(".elements-container")
+const doctorContainer = document.getElementById("elements-container")
 const collectionName = "pacientes"
 
 // Import the functions you need from the SDKs you need
@@ -26,8 +26,17 @@ const db = getFirestore(app);
 const getData = async () => {
     const querySnapshot = await getDocs(collection(db, collectionName));
     querySnapshot.forEach((paciente) => {
-    pacientes.push(paciente.data())
-    //console.log(doctor.id, " => ", doctor.data());
+        pacientes.push(paciente)
+        const nestedCollectionRef = collection(paciente.ref, "estudios");
+        getDocs(nestedCollectionRef).then((nestedSnapshot) => {
+        nestedSnapshot.forEach((nestedDocument) => {
+            // Access fields of the nested document
+            const nestedData = nestedDocument.data();
+            //console.log("Nested Document ID:", nestedDocument.id);
+            //console.log("Nested Field Value:", nestedData.descripcion);
+        });
+        });
+    
     });
     // Call the function to handle the retrieved data
     handleDataPacients();
@@ -41,38 +50,38 @@ getData().catch((error) => {
 
 const handleDataPacients = () => {
     pacientes.forEach((pas) => {
-    console.log("Cedula: ", pas.cedula);
-    console.log("Nombre: ", pas.nombre);
-    console.log("Especialidad: ", pas.especialidad);
-    crearPaciente(pas)
+    let estudiosID = pas.id
+    console.log(estudiosID)
+    pas = pas.data()
+    console.log(pas.nombre)
+    console.log(pas.edad)
+    crearPaciente(pas,estudiosID)
     // You can access other properties of the doc object here
   });
 };
 
-function crearPaciente(pas){
-    const row = document.createElement('div')
-    row.classList.add('row')
+function crearPaciente(pas,estudiosID){
+    
     
     const divContainer = document.createElement('div')
-    divContainer.classList.add('col-sm-6')
+    divContainer.classList.add('col')
 
     const cardContainer = document.createElement('div')
     cardContainer.classList.add('card-container')
 
-    const card = document.createElement('div')
+  const card = document.createElement('div')
     card.classList.add("card")
-    card.classList.add("text-bg-primary")
+    card.classList.add("text-bg-secondary")
     card.style.maxWidth = "18rem"
 
     const cardBody = document.createElement('div')
     cardBody.classList.add('card-body')
-    cardBody.classList.add('text-bg-primary')
     cardBody.classList.add('mb-3')
 
     const cardTitle = document.createElement('h5')
     cardTitle.classList.add('card-header')
     cardTitle.classList.add('fs-3')
-    cardTitle.textContent = pas.nombre
+    cardTitle.textContent = pas.nombre.toString().toUpperCase()
 
     const cardList = document.createElement('ul')
     cardList.classList.add('list-group')
@@ -80,6 +89,7 @@ function crearPaciente(pas){
 
     const cardListElement = document.createElement('li')
     cardListElement.classList.add('list-group-item')
+    cardListElement.classList.add('text-bg-secondary')
     cardListElement.style.margin = "0px"
     cardListElement.style.padding = "0px"
 
@@ -88,14 +98,46 @@ function crearPaciente(pas){
     peso.style.padding = "0px"
     peso.classList.add('card-text')
     peso.classList.add('fs-4')
-    peso.textContent = `Peso: ${pas.peso}kg`
+    peso.innerHTML = `<i class="fa-solid fa-weight-scale"></i> Peso: ${pas.peso}kg`
 
     const edad = document.createElement('p')
     edad.style.margin = "0px"
     edad.style.padding = "0px"
     edad.classList.add('card-text')
     edad.classList.add('fs-4')
-    edad.textContent = `Edad: ${pas.edad} años`
+    edad.innerHTML = `<i class="fa-solid fa-user-injured"></i> Edad: ${pas.edad} años`
+
+    const btnGroup = document.createElement('div')
+    btnGroup.classList.add('btn-group')
+    btnGroup.classList.add('p-1')
+    btnGroup.setAttribute('role','button')
+
+    const btnActualizar = document.createElement('button')
+    btnActualizar.classList.add('btn')
+    btnActualizar.classList.add('btn-warning')
+    btnActualizar.setAttribute('type','submit')
+    btnActualizar.setAttribute('data-bs-toggle','modal')
+    btnActualizar.setAttribute('data-bs-target','#editModal')
+    btnActualizar.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>'
+
+    const btnEliminar = document.createElement('button')
+    btnEliminar.classList.add('btn')
+    btnEliminar.classList.add('btn-danger')
+    btnEliminar.setAttribute('type','submit')
+    btnEliminar.innerHTML = '<i class="fa-solid fa-trash"></i>'
+
+
+    const button = document.createElement("button");
+    button.textContent = "Estudios";
+    button.classList.add("btn");
+    button.classList.add("btn-dark");
+    button.addEventListener("click", () => {
+        // Call function to retrieve and display data from the second collection
+        retrieveSecondCollectionData(estudiosID);
+    });
+
+    btnGroup.appendChild(btnActualizar)
+    btnGroup.appendChild(btnEliminar)
 
     cardListElement.appendChild(peso)
     cardListElement.appendChild(edad)
@@ -105,14 +147,108 @@ function crearPaciente(pas){
 
     card.appendChild(cardTitle)
     card.appendChild(cardBody)
+    card.appendChild(btnGroup)
+    card.appendChild(button);
 
     cardContainer.appendChild(card)
-    row.appendChild(cardContainer)
-    doctorContainer.appendChild(row)
+    doctorContainer.appendChild(crearModalEdit(pas.nombre,pas.peso,pas.edad))
+    doctorContainer.appendChild(cardContainer)
 }
 
-fetch("navBar.html").then(response => response.text()).then(data => {
-        document.getElementById("navbar-placeholder").innerHTML = data;
-    }).catch(error => {
-        console.error("Error fetching navbar:", error);
+const retrieveSecondCollectionData = async (pacienteId) => {
+    const querySnapshot = await getDocs(
+      collection(db, "pacientes", pacienteId, "estudios")
+    );
+    let nestedInfoHTML = "";
+    querySnapshot.forEach((document) => {
+      const data = document.data();
+      //console.log("ID consulta:", document.id);
+      //console.log("Diagnostico:", data.diagnostico);
+
+      nestedInfoHTML += `<p>Estudio ID: ${document.id}</p>><p> Descripción ${data.descripcion}</p><p> Fecha: ${data.fecha}</p>><p> Estudio: ${data.nombre}</p>`;
+    });
+
+    // Set the nested collection information in the modal dialog
+    const nestedInfoElement = document.getElementById("nestedInfo");
+    nestedInfoElement.innerHTML = nestedInfoHTML;
+  
+    // Show the modal dialog
+    const modal = document.getElementById("myModal");
+    modal.style.display = "block";
+};
+const closeBtn = document.getElementsByClassName("close")[0];
+closeBtn.addEventListener("click", function () {
+    const modal = document.getElementById("myModal");
+    modal.style.display = "none";
+});
+
+function crearModalEdit(nombre,cedula,especialidad){
+
+    // Modal
+
+    const divModalFade = document.createElement('div')
+    divModalFade.classList.add('modal')
+    divModalFade.classList.add('fade')
+    divModalFade.setAttribute('id', 'editModal')
+    divModalFade.setAttribute('tabindex', '-1')
+    divModalFade.setAttribute('aria-hidden', 'true')
+
+    const modalDialog = document.createElement('div')
+    modalDialog.classList.add('modal-dialog')
+
+    const modalContent = document.createElement('div')
+    modalContent.classList.add('modal-content')
+
+    const modalHeader = document.createElement('div')
+    modalHeader.classList.add('modal-header')
+
+    const modalTitle = document.createElement('h2')
+    modalTitle.classList.add('modal-title')
+    modalTitle.classList.add('fs-5')
+    modalTitle.innerHTML = nombre
+
+    const modalEditBtnClose = document.createElement('button')
+    modalEditBtnClose.classList.add('btn-close')
+    modalEditBtnClose.setAttribute('data-bs-dismiss','modal')
+    modalEditBtnClose.setAttribute('aria-label','Close')
+
+    const editModalBody = document.createElement('div')
+    editModalBody.classList.add('modal-body')
+    editModalBody.innerHTML = `<input class="form-control" value="${nombre}"><input class="form-control" value="${cedula}"><input class="form-control" value="${especialidad}">`
+
+    const editModalFooter = document.createElement('div')
+    editModalFooter.classList.add('modal-footer')
+
+    const btnCloseModalFooter = document.createElement('button')
+    btnCloseModalFooter.classList.add('btn')
+    btnCloseModalFooter.classList.add('btn-secondary')
+    btnCloseModalFooter.setAttribute('data-bs-dismiss','modal')
+    btnCloseModalFooter.innerText = "Cerrar"
+
+    const btnGuardarModalFooter = document.createElement('button')
+    btnGuardarModalFooter.classList.add('btn')
+    btnGuardarModalFooter.classList.add('btn-primary')
+    btnGuardarModalFooter.innerText = "Guardar Cambios"
+
+    editModalFooter.appendChild(btnCloseModalFooter)
+    editModalFooter.appendChild(btnGuardarModalFooter)
+    modalHeader.appendChild(modalTitle)
+    modalHeader.appendChild(modalEditBtnClose)
+    modalContent.appendChild(modalHeader)
+    modalContent.appendChild(editModalBody)
+    modalContent.appendChild(editModalFooter)
+    modalDialog.appendChild(modalContent)
+    divModalFade.appendChild(modalDialog)
+    
+    // End of modal
+    return  divModalFade
+}
+
+fetch("navBar.html")
+    .then(response => response.text())
+    .then(data => {
+    document.getElementById("navbar-placeholder").innerHTML = data;
+    })
+    .catch(error => {
+    console.error("Error fetching navbar:", error);
     });
